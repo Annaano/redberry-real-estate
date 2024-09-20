a735e71d009a732d9a6d9ff3b299d5ee43e3ee1c
Anano Romashvili <anano.romashvili@prestele.group>
2024-09-20 18:48:12 +0400

    added card component skeleton

---
+++ src/components/Card.vue
@@ -0,0 +1,41 @@
+<template>
+    <div
+        class="col-span-1 h-[455px] bg-white shadow-lg flex flex-col justify-start items-start rounded-2xl overflow-hidden"
+    >
+        <div
+            class="w-full h-[307px] border border-green-500 flex justify-center items-center relative"
+        >
+            <p class="text-black text-2xl">Image Goes Here</p>
+        </div>
+        <div class="w-full flex-grow p-6 space-y-5">
+            <div
+                class="w-full h-auto flex flex-col justify-start items-start gap-[6px]"
+            >
+                <p class="text-black font-bold text-[28px]">80 000$</p>
+                <div class="w-auto h-auto flex space-x-1">
+                    <Icon icon="marker" size="size-5" color="text-gray" />
+                    <p class="text-gray">თბილისი, ი. ჭავჭავაძის 53</p>
+                </div>
+            </div>
+            <div
+                class="w-full h-auto flex justify-start items-center space-x-8"
+            >
+                <span class="flex justify-start items-center space-x-[5px]">
+                    <Icon icon="bed" size="size-6" color="text-gray" />
+                    <p class="text-gray">2</p>
+                </span>
+                <span class="flex justify-start items-center space-x-[5px]">
+                    <Icon icon="area" size="size-6 mt-2" color="text-gray" />
+                    <p class="text-gray">54</p>
+                </span>
+                <span class="flex justify-start items-center space-x-[5px]">
+                    <Icon icon="postal" size="size-6" color="text-gray" />
+                    <p class="text-gray">0127</p>
+                </span>
+            </div>
+        </div>
+    </div>
+</template>
+<script setup>
+import Icon from '@/components/Icon.vue'
+</script>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,6 +1,12 @@
 <template>
   <Header />
+  <div class="w-full h-auto flex justify-center items-center mt-8">
+      <div class="w-4/5 h-auto grid grid-cols-3 gap-6">
+          <Card />
+      </div>
+  </div>
 </template>
 <script setup>
 import Header from '@/components/Header.vue'
+import Card from '@/components/Card.vue'
 </script>
