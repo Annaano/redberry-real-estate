5b75333674e427fb291f356623e5294ae57e924d
Anano Romashvili <anano.romashvili@prestele.group>
2024-09-20 16:01:21 +0400

    added button component but not finished yet

---
+++ src/components/Button.vue
@@ -0,0 +1,6 @@
+<template>
+    <button class="w-auto h-12 px-[10px] gap-2 text-white">{{ title }}</button>
+</template>
+<script setup>
+const props = defineProps(['title', 'color', 'disabled', 'icon'])
+</script>
