191dcf99c32a0af03b35deba05cde9c9ba481ce5
Anano Romashvili <anano.romashvili@prestele.group>
2024-09-20 15:47:21 +0400

    initial commit

---
+++ src/router/index.js
@@ -0,0 +1,9 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Index from "@/pages/Index.vue";
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [{ path: "/", name: "index", component: Index }],
+});
+
+export default router;
